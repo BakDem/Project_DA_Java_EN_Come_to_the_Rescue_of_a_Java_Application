d5b49f916392bc8072f46040335a5fd6881dc290
BakDem <cissebakarydembele@gmail.com>
2022-06-21 03:18:48 +0200

    Analyse du code existant et premier nettoyage

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,64 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+/**
+ * AnalyticsCounter the main class for :
+ * 
+ * Read a file with symptom
+ * 
+ * Count all symptom
+ * 
+ * and write the symptom with their occurrences on another file.
+ * 
+ */
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+	public static void main(String args[]) throws FileNotFoundException {
+		int symptomOccurrences = 0;
+		ArrayList<String> oneSymptom = new ArrayList<String>();
 
-			line = reader.readLine();	// get another symptom
+		ReadSymptomDataFromFile reader = new ReadSymptomDataFromFile("symptoms.txt");
+		List<String> symptomList = reader.GetSymptoms(); // Get a list with all symptoms
+
+		Collections.sort(symptomList); // sort the list into ascending order
+
+		// make a list with only one symptom. No duplicate of symptom
+		for (String line : symptomList) {
+
+			if (!oneSymptom.contains(line)) {
+				oneSymptom.add(line);
+			}
 		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+
+		/**
+		 * here Using FilleWriter Class to write on file
+		 * 
+		 * use ArrayList(symptomList) with all symptom and ArrayList(oneSymptom)with
+		 * only one symptom at time
+		 * 
+		 * Go through oneSymptom and count occurrences from symptomList
+		 * 
+		 * Concatenate the symptom and occurrences and write it on result.out.
+		 */
+		try {
+			FileWriter writer = new FileWriter("result.out");
+			for (String str : oneSymptom) {
+				symptomOccurrences = Collections.frequency(symptomList, str);
+				writer.write(str + " = " + symptomOccurrences + "\n");
+			}
+
+			writer.close();
+		} catch (
+
+		IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
